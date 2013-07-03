201668f0a0a22bc644e8a20acdd27be31993e69c
CheezeCake <emmanuel.nicolet@gmail.com>
2013-07-03 21:52:48 +0200

    todo: "debordements"

--- srtshift.pl
+++ srtshift.pl
@@ -27,27 +27,40 @@
 use strict;
 #use diagnostics;
 
+sub add_leading_zeros
+{
+    my ($val, $n) = @_;
+
+    for(my $i = 0; $i < $n; $i++)
+    {
+        $val = '0'.$val;
+    }
+
+    return $val;
+}
+
 my $ARGC = scalar @ARGV;
 (($ARGC >= 3) and ($ARGC <= 4)) or die "invalid argument list.\n";
 
 ($ARGV[0] =~ /\+|-/) or die "invalid argument: $ARGV[0] (expected + or -).\n";
 my $sign = $ARGV[0];
 
-my %units = (
-                "h" => "h",
-                "m" => "m[^s]",
-                "s" => "s",
-                "ms" => "ms"
-            );
-my %values;
+my @units = ("h", "m([^s]|\$)", "s", "ms");
+my @values;
 
-while((my $symbol, my $exp) = each %units)
+foreach my $exp (@units)
 {
     my $val = $ARGV[1];
 
-    $val =~ s/^(\d+\D)*(\d+)$exp.*$/$2/ or $val = 0;
-    $values{$symbol} = $val;
+    $val =~ s/^(\d+\D+)*(\d+)$exp.*$/$2/ or $val = 0;
+    push @values, $val;
 }
+
+foreach my $n (@values)
+{
+    print $n, "\n";
+}
+print "\n";
 
 (-f $ARGV[2]) or die "$ARGV[2]: no such file.\n";
 my $in = $ARGV[2];
@@ -55,8 +68,45 @@ my $out = ($ARGC == 4) ? $ARGV[3] : "new_".$ARGV[2];
 
 
 open IN, "< $in" or die "cannot open $in: $!.\n";
-while (my $line = <IN>)
+while(my $line = <IN>)
 {
-    ;
+    if($line =~ s/^(\d\d):(\d\d):(\d\d),(\d{3}).*(\d\d):(\d\d):(\d\d),(\d{3})(.*)$/$1#$2#$3#$4#$5#$6#$7#$8#$9/)
+    {
+        chomp $line;
+        my @tokens = split "#", $line;
+        my $nline = "";
+        for(my $i = 4; $i >= 0; $i -= 4)
+        {
+            $nline = (($i == 0) ? ' --> ' : $tokens[8]).$nline;
+            for(my $j = 3; $j >= 0; $j--)
+            {
+                my $n = ($sign eq '-') ? -$values[$j] : $values[$j];
+                my $val = $tokens[$i+$j]+$n;
+
+                if($j == 2)
+                {
+                    if(length $val == 1) { $val = &add_leading_zeros($val, 1); }
+                    $nline = $val.','.$nline;
+                }
+                elsif($j != 3)
+                {
+                    if(length $val == 1) { $val = &add_leading_zeros($val, 1); }
+                    $nline = $val.':'.$nline;
+                }
+                else
+                {
+                    my $length = length $val;
+                    if($length != 3) { $val = &add_leading_zeros($val, 3-$length); }
+                    $nline = $val.$nline;
+                }
+            }
+        }
+
+        print $nline, "\n";
+    }
+    else
+    {
+        print $line;
+    }
 }
 close IN;
